5e2b77235b5b29300459abc3dd022970550db63f
moha750 <104118616+moha750@users.noreply.github.com>
2023-04-20 23:41:44 +0300

    Add files via upload

---
+++ three.js
@@ -0,0 +1,31 @@
+var canvas = document.getElementById('canvas')
+var ctx = canvas.getContext('2d')
+var nameInput = document.getElementById('name')
+var downloadBtn = document.getElementById('download-btn')
+
+var image = new Image()
+image.crossOrigin="anonymous";
+image.src = 'three.jpg'
+image.onload = function () {
+	drawImage()
+}
+
+function drawImage() {
+	// ctx.clearRect(0, 0, canvas.width, canvas.height)
+	ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
+	ctx.font = '80px rb_b'
+	ctx.fillStyle = '#EFEAE1'
+    // ctx.fillText("Text Center", 150 ,80);
+    var textString = (nameInput.value)
+textWidth = ctx.measureText(textString).width
+ctx.fillText(textString, (canvas.width/2) - (textWidth / 2), 440);
+}
+
+nameInput.addEventListener('input', function () {
+	drawImage()
+})
+
+downloadBtn.addEventListener('click', function () {
+	downloadBtn.href = canvas.toDataURL('image/jpg')
+	downloadBtn.download = 'مُعايدة نادي أدِيب'
+})
